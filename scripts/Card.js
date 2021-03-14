c4d4f19db3a047dd1ac28da31a7ec2d92e0bd4f9
Vladimir Mishin <drli396@yandex.ru>
2021-03-14 21:50:04 +0300

    refactor: Card - creat func _toggleLike _removeCard _setEventListeners - to func have one responsibility

--- scripts/Card.js
+++ scripts/Card.js
@@ -9,21 +9,25 @@ export class Card {
     this._name = item['name'];
     this._link = item['link'];
   }
-
-  
+  // Реагирование на кнопку лайк
+  _toggleLike() {
+    this._likeIcon.classList.toggle('card__like_choosed');
+  }
+  // Корзина
+  _removeCard() {
+    this._card.remove();
+    this._card = null;
+  }
+  _setEventListeners() {
+    this._likeIcon.addEventListener('click', () => this._toggleLike());
+    this._trashCard.addEventListener('click', () => this._removeCard());
+  }
   // Создать карточку места с фото
   createCard() {
     this._imageCard.alt = this._name;
     this._imageCard.src = this._link;
     this._captionCard.textContent = this._name;
-
-    // Реагирование на кнопку лайк
-    this._likeIcon.addEventListener('click', () => { this._likeIcon.classList.toggle('card__like_choosed') });
-    // Корзина
-    this._trashCard.addEventListener('click', () => {
-      this._card.remove();
-      this._card = null;
-    });
+    this._setEventListeners();
     return this._card;
   }
 }
